158f9430675e781f03fc5f7afdc0ea410ae41940
rhos0129 <rhos0129@gmail.com>
2023-11-05 21:08:47 +0900

    add : 페이지 추가 및 route 설정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
+import Login from './pages/Login'
+import Profile from './pages/Profile'
+import ArticleList from './pages/ArticleList'
+import ArticleView from './pages/ArticleView'
+import ArticleForm from './pages/ArticleForm'
+import ArticleEdit from './pages/ArticleEdit'
+import NotFound from './pages/NotFound'
+
 
 const App = () => {
-  return (
-    <h1>Hello World!</h1>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/signup' element={<SignUp />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/profile' element={<Profile />} />
+                <Route path='/articles' element={<ArticleList />} />
+                <Route path='/article/:no' element={<ArticleView />} />
+                <Route path='/article/wirte' element={<ArticleForm />} />
+                <Route path='/article/:no/edit' element={<ArticleEdit />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
---
+++ src/pages/ArticleForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ArticleForm = () => {
+  return (
+    <div>ArticleForm</div>
+  )
+}
+
+export default ArticleForm
