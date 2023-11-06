d6e948cd22fbdd1bb8e88b2cd4fe5c673119bcf4
rhos0129 <rhos0129@gmail.com>
2023-11-06 19:59:58 +0900

    build : 라이브러리 bootstrap 추가

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './assets/scss/style.scss';
 
+// bootstrap css
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
