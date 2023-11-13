0c555420e054ae065004623ca314c63770ec474f
rhos0129 <rhos0129@gmail.com>
2023-11-14 03:00:12 +0900

    refactor : 헤더 ButtonGroup 컴포넌트 추가 및 코드 스타일 정리

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import { Stack, Button } from 'react-bootstrap'
-import Logo from 'components/common/Logo';
-import DefaultContainer from 'components/layout/DefaultContainer';
-import SimpleNav from 'components/layout/header/SimpleNav';
-import SearchBox from 'components/common/SearchBox';
+import React from "react";
+import Logo from "components/common/Logo";
+import DefaultContainer from "components/layout/DefaultContainer";
+import SimpleNav from "components/layout/header/SimpleNav";
+import SearchBox from "components/common/SearchBox";
+import ButtonGroup from "components/common/ButtonGroup";
 
 const Header = () => {
-
   const navItems = [
     {
       title: "QnA",
@@ -20,28 +19,35 @@ const Header = () => {
       title: "일상",
       href: "/articles/life",
     },
-  ]
+  ];
+
+  const buttonGroupItems = [
+    {
+      text: "로그인",
+      href: "/login",
+      color: "light",
+    },
+    {
+      text: "회원가입",
+      href: "/signup",
+      color: "dark",
+    },
+  ];
 
   return (
-    <header id='header' role='banner'
-      className='sticky-top h-100 bg-white border-bottom border-2 mb-3'>
+    <header
+      id="header"
+      role="banner"
+      className="sticky-top h-100 bg-white border-bottom border-2 mb-3"
+    >
       <DefaultContainer row>
         <Logo />
         <SimpleNav items={navItems} />
-        <SearchBox placeholder='전체 검색' width='40%' />
-        <Stack direction='horizontal' gap={1} >
-          <Button className='rounded-5 border-bluegray-900 bg-white text-black fs-sm'
-            href='/login' >
-            로그인
-          </Button>
-          <Button className='rounded-5 bg-bluegray-900 border-bluegray-900 fs-sm'
-            href='/signup'>
-            회원가입
-          </Button>
-        </Stack>
+        <SearchBox placeholder="전체 검색" width="40%" />
+        <ButtonGroup items={buttonGroupItems} rounded />
       </DefaultContainer>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/layout/header/SimpleNav.jsx
+++ src/components/layout/header/SimpleNav.jsx
@@ -1,33 +1,19 @@
-import React from 'react'
-import { Nav } from 'react-bootstrap'
-import { useLocation } from 'react-router';
-
+import React from "react";
+import { Nav } from "react-bootstrap";
+import { useLocation } from "react-router";
 
 const SimpleNav = ({ items }) => {
-
   const location = useLocation();
 
   return (
     <Nav activeKey={location.pathname}>
-      {
-        items.map((item, key) => (
-          <Nav.Link href={item.href} key={key}>
-            {item.title}
-          </Nav.Link>
-        ))
-      }
+      {items.map((item, key) => (
+        <Nav.Link href={item.href} key={key}>
+          {item.title}
+        </Nav.Link>
+      ))}
     </Nav>
-  )
-}
-
-SimpleNav.defaultProps = {
-  items: [
-    {
-      title: 'null',
-      href: 'null',
-    },
-  ],
+  );
 };
 
-
-export default SimpleNav
+export default SimpleNav;
