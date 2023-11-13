5bbade855ccbfda6269fb04f2d5c4691c5960d8d
rhos0129 <rhos0129@gmail.com>
2023-11-13 16:47:12 +0900

    build : 라이브러리 react-quill 추가

--- src/pages/article/ArticleForm.jsx
+++ src/pages/article/ArticleForm.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import Layout from 'components/layout/Layout'
+import React from "react";
+import Layout from "components/layout/Layout";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const ArticleForm = () => {
+  const modules = {
+    toolbar: [
+      [{ header: 1 }, { header: 2 }],
+      ["bold", "italic", "underline", "strike"],
+      [{ list: "ordered" }, { list: "bullet" }],
+      ["blockquote", "code", "code-block", "link", "image", "divider"],
+    ],
+  };
+
   return (
     <Layout>
-      ArticleForm
+      <ReactQuill
+        className="w-100 my-5"
+        theme="snow"
+        modules={modules}
+        placeholder="react-quill test"
+        style={{height: '50vh'}}
+      />
     </Layout>
-  )
-}
+  );
+};
 
-export default ArticleForm
+export default ArticleForm;
