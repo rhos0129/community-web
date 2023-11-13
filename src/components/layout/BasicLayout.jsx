c959c8bc2f843f17ff2d599b7942b72c26b91778
rhos0129 <rhos0129@gmail.com>
2023-11-14 03:43:50 +0900

    feat : layout에 반응형 적용

--- src/components/layout/BasicLayout.jsx
+++ src/components/layout/BasicLayout.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import Header from 'components/layout/header/Header'
-import Footer from 'components/layout/footer/Footer'
-import DefaultContainer from 'components/layout/DefaultContainer'
+import React from "react";
+import Header from "components/layout/header/Header";
+import Footer from "components/layout/footer/Footer";
+import DefaultContainer from "components/layout/DefaultContainer";
 
-const BasicLayout = ({children}) => {
+const BasicLayout = ({ children }) => {
   return (
     <>
       <Header />
-      <main id='main' role='main'>
-        <DefaultContainer>
+      <main id="main" role="main">
+        <DefaultContainer responsive>
           <div></div>
           {children}
           <div></div>
@@ -16,7 +16,7 @@ const BasicLayout = ({children}) => {
       </main>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default BasicLayout
+export default BasicLayout;
--- src/components/layout/DefaultContainer.jsx
+++ src/components/layout/DefaultContainer.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import { Col, Container } from 'react-bootstrap'
+import React from "react";
+import { Col, Container } from "react-bootstrap";
 
-const DefaultContainer = ({ children, row }) => {
+const DefaultContainer = ({ children, row, responsive }) => {
   return (
-    <Container fluid className='d-flex align-items-center'>
-      <Col className='d-flex justify-content-start'>
+    <Container fluid={"lg"} className="d-flex align-items-center">
+      <Col className={"d-flex justify-content-start"}>
         {children.slice(0, 1)}
       </Col>
-      <Col className={'d-flex ' + (row ? 'justify-content-between' : 'flex-column align-items-center')} md={7} >
+      <Col
+        className={
+          "d-flex" +
+          (row
+            ? " justify-content-between "
+            : " flex-column align-items-center ") +
+          (responsive && "col-12")
+        }
+        lg={8}
+      >
         {children.slice(1, -1)}
       </Col>
-      <Col className='d-flex justify-content-end'>
-        {children.slice(-1)}
-      </Col>
+      <Col className={"d-flex justify-content-end"}>{children.slice(-1)}</Col>
     </Container>
-  )
-}
+  );
+};
 
-export default DefaultContainer
+export default DefaultContainer;
